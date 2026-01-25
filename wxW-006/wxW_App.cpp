86f81dc1df8a85ab1bb30dcdfc31e21b9d9280f3
DrabekDD <kyczbard@gmail.com>
2026-01-25 11:27:02 +0100

    + struktura obiektów wxW

--- wxW-006/wxW_App.cpp
+++ wxW-006/wxW_App.cpp
@@ -2,10 +2,20 @@
 #include "wxW_Main.h"
 
 const wxString appName = "wxW - tworzymy menu";
+//(*AppHeaders
+#include <wx/image.h>
+//*)
+
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
+	//(*AppInitialize
+	bool wxsOK = true;
+	wxInitAllImageHandlers();
+	//*)
+	return wxsOK;
+
     MyFrame* frame = new MyFrame(NULL, wxID_ANY, appName, wxPoint(200, 150),
         wxSize(900, 700), wxDEFAULT_FRAME_STYLE | wxRESIZE_BORDER
     );
